b36fafc12e048a4a39fae772ed03db17449c0739
sfigiel <szymon.figiel@slabs.pl>
2020-02-27 11:03:04 +0100

    implemented connexion

--- server.py
+++ server.py
@@ -2,9 +2,16 @@ from flask import (
     Flask,
     render_template
 )
+import connexion
 
-# Create the application instance
-app = Flask(__name__, template_folder="templates")
+# # Create the application instance
+# app = Flask(__name__, template_folder="templates")
+
+# creating the application instance using Connexion rather than Flask
+app = connexion.App(__name__, specification_dir="./")
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -17,6 +24,5 @@ def home():
     """
     return render_template('home.html')
 
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
